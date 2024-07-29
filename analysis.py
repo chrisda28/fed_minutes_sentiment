26faf5476530617c48b1753667d05d9b49b8567c
chrisda28 <chrisda28@gmail.com>
2024-07-29 16:28:31 -0400

    create function names and important keywords

--- analysis.py
+++ analysis.py
@@ -1,7 +1,38 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from tqdm.notebook import tqdm
-sia = SentimentIntensityAnalyzer()
+import pandas as pd
+
+INPUT_FOLDER = "fed_minutes"
+KEYWORDS = ['inflation', 'labor', 'growth',
+            'unemployment', 'deficit', 'surplus',
+            'export', 'import', 'job', 'jobs',
+            'consumption', 'investment', 'trade']
+sia = SentimentIntensityAnalyzer()   # initialize VADER model
 
 boo = sia.polarity_scores("I like going to the beach.")
 print(boo)
+
+
+def analyze_files():
+    pass
+
+
+def keyword_count():
+    pass
+
+
+def single_meeting_sentiment():
+    pass
+
+
+def keyword_trend_plot():
+    pass
+
+
+def sentiment_plot():
+    pass
+
+
+def make_word_cloud():
+    pass
