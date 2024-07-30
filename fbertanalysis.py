b02d8363b5983a63266c5f1c63b34e1f609768a9
chrisda28 <chrisda28@gmail.com>
2024-07-30 15:52:25 -0400

    get finbert model working and used a lot of code from helpful tutorial video https://www.youtube.com/watch?v=hgg2GAgDLzA

--- fbertanalysis.py
+++ fbertanalysis.py
@@ -8,6 +8,44 @@ model = BertForSequenceClassification.from_pretrained('ProsusAI/finbert')
 with open(file="fed_minutes/fomcminutes20220126.txt", mode="r", encoding="utf-8") as file:
     result = file.read()
     tokens = tokenizer.encode_plus(result, add_special_tokens=False, return_tensors="pt")
-    input_id_chunks = tokens['input_ids'][0].split(510)
-    attention_mask_chunks = tokens['attention_mask'][0].split(510)
-    print(len(attention_mask_chunks))
+    chunksize = 512
+    input_id_chunks = list(tokens['input_ids'][0].split(chunksize - 2))
+    attention_mask_chunks = list(tokens['attention_mask'][0].split(chunksize - 2))
+
+    for i in range(len(input_id_chunks)):
+        input_id_chunks[i] = torch.cat([
+            torch.tensor([101]), input_id_chunks[i], torch.tensor([102])
+        ])
+
+        attention_mask_chunks[i] = torch.cat([
+            torch.tensor([1]), attention_mask_chunks[i], torch.tensor([1])
+        ])
+
+        pad_length = chunksize - input_id_chunks[i].shape[0]
+
+        if pad_length > 0:
+            input_id_chunks[i] = torch.cat([
+                input_id_chunks[i], torch.Tensor([0] * pad_length)
+            ])
+
+            attention_mask_chunks[i] = torch.cat([
+                attention_mask_chunks[i], torch.Tensor([0] * pad_length)
+            ])
+
+    input_ids = torch.stack(input_id_chunks)
+    attention_mask = torch.stack(attention_mask_chunks)
+
+    input_dict = {
+        'input_ids': input_ids.long(),
+        'attention_mask': attention_mask.int()
+    }
+
+
+    # use input_dict[‘input_ids’].shape  to see that all windows are 512, first number in output is batch size
+    outputs = model(**input_dict)
+
+    probabilities = torch.nn.functional.softmax(outputs[0], dim=-1)
+
+    mean_probabilities = probabilities.mean(dim=0)
+
+    print(torch.argmax(mean_probabilities).item())  # gives single classification (0 ,1, or 2)  2 is neutral
