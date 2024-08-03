9bd6acf00212c1348d839dbf2816105938d21dfd
chrisda28 <chrisda28@gmail.com>
2024-08-02 20:59:36 -0400

    get functionality working in fbert file by making sure tensor data is compatible. implement main file functionality giving the user a simple interface to navigate with

--- fbertanalysis.py
+++ fbertanalysis.py
@@ -8,8 +8,8 @@ import seaborn as sb
 FOLDER_PATH = "fed_minutes"
 MODEL_NAME = "ProsusAI/finbert"
 KEYWORDS = ['inflation', 'interest rates', 'unemployment', 'gdp growth', 'monetary policy',
-            'fiscal policy', 'quantitative easing', 'quantitative tightening', 'labor market', 'consumer spending',
-            'supply chain', 'financial stability', 'deficit', 'surplus', 'yield']
+            'fiscal policy', 'debt', 'liquidity', 'labor market', 'consumer spending',
+            'supply chain', 'financial stability', 'deficit', 'surplus', 'yield', 'leverage', 'housing', 'mortgage']
 
 
 def load_model_and_tokenizer():
@@ -30,45 +30,49 @@ def extract_keywords(keywords, text):  # LIMITATION: currently finds partial mat
 
 
 def get_sentiment_dict(tokenizer, text, chunksize=512):  # chunksize max is 512
-    """prepares text for sentiment analysis"""
     tokens = tokenizer.encode_plus(text, add_special_tokens=False, return_tensors="pt")
     # tokenizing text, returns PT Tensors
-    input_id_chunks = list(tokens['input_ids'][0].split(chunksize - 2))    # splitting each chunk into 510 tokens
+    input_id_chunks = list(tokens['input_ids'][0].split(chunksize - 2))  # splitting each chunk into 510 tokens
     attention_mask_chunks = list(tokens['attention_mask'][0].split(chunksize - 2))
 
     for i in range(len(input_id_chunks)):  # iterating over each chunk
         input_id_chunks[i] = torch.cat([  # Add special tokens: 101 (CLS) at start and 102 (SEP) at end of each chunk
             torch.tensor([101]), input_id_chunks[i], torch.tensor([102])
         ])
-
         attention_mask_chunks[i] = torch.cat([  # Add attention mask values for special tokens
             torch.tensor([1]), attention_mask_chunks[i], torch.tensor([1])
         ])
         # Calculate how much padding is needed to reach the chunksize
         pad_length = chunksize - input_id_chunks[i].shape[0]
 
-        if pad_length > 0:    # chunk is not 512, filling in padding so all chunks are the max token length (512)
+        if pad_length > 0:  # chunk is not 512, filling in padding so all chunks are the max token length (512)
             input_id_chunks[i] = torch.cat([
-                input_id_chunks[i], torch.Tensor([0] * pad_length)
+                input_id_chunks[i], torch.tensor([0] * pad_length, dtype=torch.long)
             ])
-            #  Add padding (0s) to input IDs and attention mask if needed
-            attention_mask_chunks[i] = torch.cat([
-                attention_mask_chunks[i], torch.Tensor([0] * pad_length)
+            attention_mask_chunks[i] = torch.cat([   #  Add padding (0s) to input IDs and attention mask if needed
+                attention_mask_chunks[i], torch.tensor([0] * pad_length, dtype=torch.long)
             ])
-    return {    # Stack chunks into single tensors, creating batches for efficient processing
-        "input_ids": torch.stack(input_id_chunks),
-        "attention_mask": torch.stack(attention_mask_chunks)
+    return {   # Stack chunks into single tensors, creating batches for efficient processing
+        "input_ids": torch.stack(input_id_chunks).long(),
+        "attention_mask": torch.stack(attention_mask_chunks).long()
     }
 # attention mask value for real token = 1 while for padding, attention_mask value = 0
 # input_ids are a numerical representation of each token
 
 
 def get_sentiment(input_dict, model):
-    """Outputs final sentiment (0, 1, or 2)"""
-    outputs = model(input_dict)  # get raw outputs from model
-    probabilities = torch.nn.functional.softmax(outputs[0], dim=-1)  # turn raw outputs into probabilities
-    mean_probabilities = probabilities.mean(dim=0)  # finding mean probability across each chunk of text
-    return torch.argmax(mean_probabilities).item()  # finding occurrence of highest probability to get sentiment score
+    all_probabilities = []
+    for i in range(input_dict['input_ids'].shape[0]):  # iterate over each sample in input batch
+        chunk_input = {
+            'input_ids': input_dict['input_ids'][i].unsqueeze(0),
+            'attention_mask': input_dict['attention_mask'][i].unsqueeze(0)
+        }
+        with torch.no_grad():  # no gradient calculation this improves efficiency
+            outputs = model(**chunk_input)
+        probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)  # use softmax to get probabilities
+        all_probabilities.append(probabilities)  # put probabilities into list
+    mean_probabilities = torch.cat(all_probabilities).mean(dim=0)
+    return torch.argmax(mean_probabilities).item()
 
 
 def analyze_doc(file_path, tokenizer, model, keywords):
@@ -101,14 +105,21 @@ def analyze_multiple_docs(tokenizer, model, ):
 def plot_keyword_heatmap(df, keywords):
     """generate heatmap showing keyword frequency for each file"""
     heatmap_data = df[keywords]
+    filenames = df['file_name'].tolist()  # turning file_names into list
 
     # Create heatmap
-    plt.figure(figsize=(12, 8))
-    sb.heatmap(heatmap_data, cmap="YlOrRd", annot=True, fmt="d")
+    plt.figure(figsize=(16, 10))
+    ax = sb.heatmap(heatmap_data, cmap="YlOrRd", annot=True, fmt="d")
 
     plt.title("Keyword Frequency Heatmap")
     plt.xlabel("Keywords")
     plt.ylabel("FOMC Minutes")
+    # Set y-tick labels to filenames and adjust their position
+    ax.set_yticks(range(len(filenames)))
+    ax.set_yticklabels(filenames, rotation=0)
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
     plt.tight_layout()
     plt.show()
 
--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 import os
 from fbertanalysis import *
 import pandas as pd
-
+# NEED: improved comments, and finalization on github repo, update portfolio site
 run_program = True
 tokenizer, model = load_model_and_tokenizer()
 df_results = analyze_multiple_docs(tokenizer, model)
 print('\nHello! Welcome to the Federal Reserve Meeting Minutes sentiment analysis.\n')
 while run_program:
     home_input = int(input("Type 0 to see the sentiment trend of all"
-                       " articles for the past 2 years. Or press 1 to see the specific articles"
-                       " and see the keyword count and sentiment for specific article"))
+                       " articles for the past 2 years.\n Type 1 to see a keyword count heatmap\n "
+                           "pertaining to all scraped articles.\n Type 2 to see the specific articles"
+                       " and see the keyword count and sentiment for specific article\n"))
     if home_input == 0:
-        plot_sentiment_trend(df_results)
+        plot_sentiment_trend(df_results)    # display sentiment trend across articles spanning past 2 years
     elif home_input == 1:
+        plot_keyword_heatmap(df_results, KEYWORDS)
+    elif home_input == 2:
         for file in os.listdir('fed_minutes'):
-            if os.path.isfile(os.path.join('fed_minutes', file)):
-                print(f"{file}")  # print all file names of scraped fed minutes articles
-                print("These are meeting minutes articles for the past 2 years."
-                      " For each file, you can either view the sentiment of the file or the keyword count heatmap.")
-                user_file_input = input("Type the filename you are interested in here: ")
-                analysis_type_input = input("Either type the word 'heatmap' or 'sentiment' here: ")
-                if analysis_type_input == 'heatmap':
-                    pass
-                elif analysis_type_input == 'sentiment':
-                    pass
-                else:
-                    print("Start over again -- yup run it all again and follow directions correctly.")
-                    run_program = False
+            print(f"{file}")  # print all file names of scraped fed minutes articles
+        print("These are meeting minutes articles for the past 2 years."
+              " For each file, you can either view the sentiment of the file or the keyword count heatmap.")
+        user_file_input = input("Type the filename you are interested in seeing sentiment for here: ")
+        keyword_count, sentiment = analyze_doc(f"fed_minutes/{user_file_input}", tokenizer, model, KEYWORDS)
+        if sentiment == 0:
+            print("Negative Sentiment")
+        elif sentiment == 1:
+            print("Neutral Sentiment")
+        elif sentiment == 2:
+            print("Positive Sentiment")
+
+        else:
+            print("Start over again and follow directions correctly.")
 
     else:
-        print("Start over again -- yup run it all again and follow directions correctly.")
-        run_program = False
+        print("Start over again follow directions correctly.")
+
